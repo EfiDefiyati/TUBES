5140f08b1faaac3f1ec17eb4f735ba64f8abe8fb
Nabyla Sharfina <nabylasharfina8@gmail.com>
2024-12-19 01:42:20 +0700

    Update linkedlist

--- kelompok5.py
+++ kelompok5.py
@@ -1,2 +1,26 @@
-print("coba")
+import tkinter as tk
+from tkinter import messagebox, simpledialog
+
+# Kelas untuk mendefinisikan Node yang menyimpan data pada linked list
+class Node:
+    def __init__(self, data):
+        self.data = data  
+        self.next = None  
+
+# Kelas untuk operasi-operasi pada LinkedList
+class LinkedList:
+    def __init__(self):
+        self.head = None  
+
+    # Fungsi untuk menambahkan node baru di akhir linked list
+    def append(self, data):
+        new_node = Node(data)  
+        if self.head is None:  
+            self.head = new_node  
+        else:
+            current = self.head  
+            while current.next:  
+                current = current.next
+            current.next = new_node  
+
 
